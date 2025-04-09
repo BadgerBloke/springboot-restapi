126e5377bb0e50444a1a19bb20e903a8559d5255
Mukesh Singh <CAMukeshKumarSingh@gmail.com>
2025-04-10 01:29:24 +0530

    feat: securing the app

--- src/main/java/learning/cashcard/CashCard.java
+++ src/main/java/learning/cashcard/CashCard.java
@@ -2,5 +2,5 @@ package learning.cashcard;
 
 import org.springframework.data.annotation.Id;
 
-record CashCard(@Id Long id, Double amount) {
+record CashCard(@Id Long id, Double amount, String owner) {
 }
